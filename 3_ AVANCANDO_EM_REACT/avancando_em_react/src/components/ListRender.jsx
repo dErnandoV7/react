926bd4cfc8c95ba867da691b07b7a7b2e48c4071
dErnandoV7 <d.ernandov7@gmail.com>
2024-01-02 07:01:18 -0300

    avancando em react part2
    propriedade key
    previous state

--- 3_ AVANCANDO_EM_REACT/avancando_em_react/src/components/ListRender.jsx
+++ 3_ AVANCANDO_EM_REACT/avancando_em_react/src/components/ListRender.jsx
@@ -1,15 +1,40 @@
-import {useState} from 'react'
+import { useState } from 'react'
 
 const ListRender = () => {
-    const [list] = useState(["Ernando", "Alexsandro", "Júnior"])
+  const [list] = useState(["Ernando", "João", "Maria", "Joana"])
+
+  const [users, setUsers] = useState([
+    { id: 0, name: "Ernando", age: 18 },
+    { id: 1, name: "João", age: 11 },
+    { id: 2, name: "Maria", age: 38 },
+    { id: 3, name: "Joana", age: 20 },
+  ])
+
+  const [msgDeleteUser, setMsgDeleteUser] = useState("Deletar usuario de forma aleatoria")
+
+  const deleteRandom = () => {
+    const randomNumber = Math.floor(Math.random() * 4)
+    setUsers((prevUsers) => prevUsers.filter((user) => randomNumber !== user.id))
+
+    if(!users.length) setMsgDeleteUser("Lista sem usuários para deletar!")
+  }
 
   return (
     <div>
-        <ul>
-            {list.map(item => (
-                <li>{item}</li>
-            ))}
-        </ul>
+      {/* Render sem key */}
+      <ul>
+        {list.map((item, i) => (
+          <li key={i}>{item}</li>
+        ))}
+      </ul>
+      {/* Render com key */}
+      <ul>
+        {users.map(user => (
+          <li key={user.id}>{user.name} - {user.age} anos</li>
+        ))}
+      </ul>
+      {/* 6. Previous state */}
+      <button onClick={deleteRandom}>{msgDeleteUser}</button>
     </div>
   )
 }
