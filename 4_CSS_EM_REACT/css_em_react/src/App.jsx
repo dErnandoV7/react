cb56ef615dd620652e2cb2724cb56709df893859
dErnandoV7 <d.ernandov7@gmail.com>
2024-01-04 08:15:44 -0300

    css em react part1
    css global
    css em componentes
    css inline

--- 4_CSS_EM_REACT/css_em_react/src/App.jsx
+++ 4_CSS_EM_REACT/css_em_react/src/App.jsx
@@ -1,33 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+// 2. CSS de componente
+import MyComponent from './components/MyComponent'
 
+function App() {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    {/* 1. CSS Global */}
+    <h1>CSS no React</h1>
+    {/* 2. CSS de componente */}
+    <MyComponent />
+    <p>Pegou o css de outro componente</p>
+    {/* 3. Estilos inline */}
+    <h2 style={{color: "blue", padding: "30px", border: "2px solid red"}} >Este elemento tem estilos inline</h2>
     </>
   )
 }
---
+++ 4_CSS_EM_REACT/css_em_react/src/components/MyComponent.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import './MyComponent.css'
+
+const MyComponent = () => {
+  return (
+    <>
+    <h1>Css do componente</h1>
+    <p>Este parágrafo vai ter um estilo</p>
+    </>
+  )
+}
+
+export default MyComponent
