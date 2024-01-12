e2da6ab0633e074eb5b71bae30df7fb498898e87
dErnandoV7 <d.ernandov7@gmail.com>
2024-01-12 13:36:41 -0300

    http em react
    resgatando dados

---
+++ 7_HTTP_REACT/http_react/src/App.jsx
@@ -0,0 +1,39 @@
+import { useState, useEffect } from 'react'
+
+const url = "http://localhost:3000/products"
+
+import './App.css'
+
+function App() {
+  // 1. Resgatando dados
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+
+    async function getData() {
+      const res = await fetch(url)
+      const data = await res.json()
+
+      setProducts(data)
+    }
+
+    getData()
+  }, [])
+
+  return (
+    <div className="App">
+      <h1>HTTP em React</h1>
+
+      {/* 1. Resgatando dados */}
+      <ul>
+        {products.map(product => (
+          <li key={product.id}>
+            {product.name} - {product.price}
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default App
